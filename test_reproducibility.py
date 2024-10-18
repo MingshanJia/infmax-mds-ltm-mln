f7422c98ec9bec512228add29620e7b8132ec846
Michał Czuba <michal.czuba.1995@gmail.com>
2024-10-18 15:31:10 +0000

    updated tests

--- test_reproducibility.py
+++ test_reproducibility.py
@@ -56,7 +56,7 @@ def test_e2e(tcase_config, tcase_csv_names, tmpdir):
     tcase_config["logging"]["out_dir"] = str(tmpdir)
     set_rng_seed(tcase_config["run"]["random_seed"])
     main.run_experiments(tcase_config)
-    compare_results(Path("_test_data"), Path(tmpdir), tcase_csv_names)
+    compare_results(Path("data/test"), Path(tmpdir), tcase_csv_names)
 
 
 if __name__ == "__main__":
